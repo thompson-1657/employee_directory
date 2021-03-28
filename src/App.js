5403b81b6d2c45bf70b725cb8a1557566dbefe1c
Rachel Thompson <thompson.1657@gmail.com>
2021-03-28 00:24:46 -0500

    installed Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react'
 
+import Header from './components/Header.js'
 import './App.css';
 
 function App() {
   return (
- <div>
-   
- </div>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className="container">
+         <div className="row" style={{textAlign: 'center'}}>
+             <div className="jumbotron">
+                <h1>Employee Directory</h1>
+                     {/* <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+                    responsive, mobile-first projects on the web.</p> */}
+             </div>
+         </div>
+        </div>
+)
+}
+
+export default Header
